4c09897b9fe8b47ffe407c90455eb8a977dc96a2
Gabriel <gabriel.tandil@gmail.com>
2011-07-17 19:24:23 -0300

    Creando webservices genericos

--- ProveedorServicios/src/ar/edu/unicen/exa/galvarez/patogis/servidor/webservices/impl/PatoGisWSAbstract.java
+++ ProveedorServicios/src/ar/edu/unicen/exa/galvarez/patogis/servidor/webservices/impl/PatoGisWSAbstract.java
@@ -1,15 +1,21 @@
 package ar.edu.unicen.exa.galvarez.patogis.servidor.webservices.impl;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
+import java.rmi.RemoteException;
 import java.sql.Connection;
 import java.sql.SQLException;
+import java.util.List;
 
 import org.apache.ibatis.session.SqlSession;
 
+import ar.edu.unicen.exa.galvarez.patogis.servidor.logica.EspecieMapper;
+import ar.edu.unicen.exa.galvarez.patogis.servidor.modelo.Especie;
+import ar.edu.unicen.exa.galvarez.patogis.servidor.modelo.EspecieExample;
 import ar.edu.unicen.exa.galvarez.patogis.servidor.util.AuditoriaUtil;
 import ar.edu.unicen.exa.galvarez.patogis.servidor.util.SqlConnection;
 
-public abstract class PatoGisWSAbstract {
+public abstract class PatoGisWSAbstract<T> {
 
 	public PatoGisWSAbstract() {
 		super();
@@ -38,4 +44,45 @@ public abstract class PatoGisWSAbstract {
 		return sqlSession;
 	}
 
+	
+	
+	public List<T> getElementos(Class<T> clase) throws RemoteException {
+		SqlSession sqlSession = null;
+		try {
+			sqlSession = obtenerSesion();
+			EspecieExample especieExample = new EspecieExample();
+
+			EspecieMapper mapper = sqlSession.getMapper(EspecieMapper.class);
+
+			List<T> allRecords = mapper.selectByExample(especieExample);
+
+			
+			return allRecords;
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+		return null;
+
+	}
+
+	public void addEspecie(Especie especie) throws RemoteException {
+		SqlSession sqlSession = null;
+		try {
+			int idUsuario=1;
+			sqlSession = obtenerSesionAuditada(idUsuario);
+			EspecieMapper mapper = sqlSession.getMapper(EspecieMapper.class);
+			mapper.insertSelective(especie);
+			liberarSesionAuditada(sqlSession);
+		} catch (Exception e) {
+
+			e.printStackTrace();
+		} finally {
+			sqlSession.close();
+		}
+
+	}	
+	
 }
