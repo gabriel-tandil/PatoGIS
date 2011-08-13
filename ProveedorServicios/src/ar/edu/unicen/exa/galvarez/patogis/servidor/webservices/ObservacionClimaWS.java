5562e7494e41897e2e7bf513f192aa2848f2d64f
Gabriel <gabriel.tandil@gmail.com>
2011-08-13 08:07:26 -0300

    Completado salvado ws observacion

---
+++ ProveedorServicios/src/ar/edu/unicen/exa/galvarez/patogis/servidor/webservices/ObservacionClimaWS.java
@@ -0,0 +1,17 @@
+package ar.edu.unicen.exa.galvarez.patogis.servidor.webservices;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+import ar.edu.unicen.exa.galvarez.patogis.servidor.modelo.ObservacionClima;
+
+public interface ObservacionClimaWS extends Remote {
+
+	// los metodos getElementos y addElemento se redefinen aca invocando al del
+	// padre porque la refinicion del web service asi lo requiere
+	public ObservacionClima[] getElementos() throws RemoteException;
+
+	public void addElemento(ObservacionClima elemento, Integer idUsuario)
+			throws RemoteException;
+
+}
--- ProveedorServicios/src/ar/edu/unicen/exa/galvarez/patogis/servidor/webservices/impl/ObservacionWSImpl.java
+++ ProveedorServicios/src/ar/edu/unicen/exa/galvarez/patogis/servidor/webservices/impl/ObservacionWSImpl.java
@@ -3,6 +3,13 @@ package ar.edu.unicen.exa.galvarez.patogis.servidor.webservices.impl;
 import java.rmi.RemoteException;
 
 import ar.edu.unicen.exa.galvarez.patogis.servidor.modelo.Observacion;
+import ar.edu.unicen.exa.galvarez.patogis.servidor.modelo.ObservacionEspecie;
+import ar.edu.unicen.exa.galvarez.patogis.servidor.modelo.ObservacionFoto;
+import ar.edu.unicen.exa.galvarez.patogis.servidor.modelo.ObservacionMatrizProductiva;
+import ar.edu.unicen.exa.galvarez.patogis.servidor.webservices.ObservacionClimaWS;
+import ar.edu.unicen.exa.galvarez.patogis.servidor.webservices.ObservacionEspecieWS;
+import ar.edu.unicen.exa.galvarez.patogis.servidor.webservices.ObservacionFotoWS;
+import ar.edu.unicen.exa.galvarez.patogis.servidor.webservices.ObservacionMatrizProductivaWS;
 import ar.edu.unicen.exa.galvarez.patogis.servidor.webservices.ObservacionWS;
 
 public class ObservacionWSImpl extends PatoGisWSAbstractImpl<Observacion>
@@ -23,5 +30,24 @@ public class ObservacionWSImpl extends PatoGisWSAbstractImpl<Observacion>
 	public void addElemento(Observacion elemento, Integer idUsuario)
 			throws RemoteException {
 		super.addElementoGenerico(elemento, idUsuario);
+		ObservacionEspecieWS oews=new ObservacionEspecieWSImpl();
+		ObservacionMatrizProductivaWS ompws=new ObservacionMatrizProductivaWSImpl();
+		ObservacionClimaWS ocws=new ObservacionClimaWSImpl();
+		ObservacionFotoWS ofws=new ObservacionFotoWSImpl();
+
+		for (ObservacionEspecie observacionEspecie : elemento.getObservacionesEspecie()) {
+			observacionEspecie.setIdObservacion(elemento.getId());
+			oews.addElemento(observacionEspecie, idUsuario);
+		}
+		for (ObservacionMatrizProductiva observacionMatrizProductiva : elemento.getObservacionesMatrizProductiva()) {
+			observacionMatrizProductiva.setIdObservacion(elemento.getId());
+			ompws.addElemento(observacionMatrizProductiva, idUsuario);
+		}
+		for (ObservacionFoto observacionFoto : elemento.getObservacionesFoto()) {
+			observacionFoto.setIdObservacion(elemento.getId());
+			ofws.addElemento(observacionFoto, idUsuario);
+		}
+		elemento.getObservacionClima().setIdObservacion(elemento.getId());
+		ocws.addElemento(elemento.getObservacionClima(), idUsuario);
 	}
 }
