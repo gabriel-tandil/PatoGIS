84323ca1eaf33d301a8d89a79deccddc35895645
Gabriel <gabriel.tandil@gmail.com>
2011-09-28 20:32:18 -0300

    avances mejora campo de hora

--- AplicacionWeb/src/ar/edu/unicen/exa/galvarez/patogis/client/TimeBox.java
+++ AplicacionWeb/src/ar/edu/unicen/exa/galvarez/patogis/client/TimeBox.java
@@ -6,183 +6,92 @@ import com.google.gwt.user.client.ui.TextBox;
 
 public class TimeBox extends TextBox {
 
-        public final static String DEFAULT_TIME_FORMAT = "HH:mm";
+	public final static String DEFAULT_TIME_FORMAT = "HH:mm";
 
-        private String format = DEFAULT_TIME_FORMAT;
+	private String format = DEFAULT_TIME_FORMAT;
 
+	public TimeBox() {
+		super.sinkEvents(Event.ONKEYDOWN);
+	}
 
-        public TimeBox() {
-                this(DEFAULT_TIME_FORMAT);
-        }
+	public TimeBox(String format) {
+		super.sinkEvents(Event.ONKEYDOWN);
+	}
 
-        public TimeBox(String format) {
-        super.sinkEvents(Event.ONKEYDOWN);
-        setFormat(format);
-    }
+	public void onBrowserEvent(Event event) {
+		int eventType = DOM.eventGetType(event);
+		int keyCode = DOM.eventGetKeyCode(event);
 
-        public void onBrowserEvent(Event event) {
-                int eventType = DOM.eventGetType(event);
-                int keyCode = DOM.eventGetKeyCode(event);
+		switch (eventType) {
+		case Event.ONKEYDOWN: {
+			if (isTimeKey(keyCode)) {
+				char newChar = ((char) convert(keyCode));
+				setText(getText() + String.valueOf(newChar));
+				if (getText().length() == 2)
+					setText(getText() + ":");
+				DOM.eventPreventDefault(event);
+				return;
+			}
+		}
+		default:
+			super.onBrowserEvent(event);
+		}
+	}
 
-                switch (eventType) {
-                case Event.ONKEYDOWN: {
+	private int convert(int keyCode) {
+		// convert 0 - 9 keys on number pad down into normal 0 - 9 range
+		if (keyCode >= 96 && keyCode <= 105) {
+			return keyCode - 48;
+		} else if (keyCode == 81) {
+			keyCode = 49;
+		} else if (keyCode == 87) {
+			keyCode = 50;
+		} else if (keyCode == 69) {
+			keyCode = 51;
+		} else if (keyCode == 82) {
+			keyCode = 52;
+		} else if (keyCode == 84) {
+			keyCode = 53;
+		} else if (keyCode == 89) {
+			keyCode = 54;
+		} else if (keyCode == 85) {
+			keyCode = 55;
+		} else if (keyCode == 37) {
+			keyCode = 56;
+		} else if (keyCode == 79) {
+			keyCode = 57;
+		} else if (keyCode == 80) {
+			keyCode = 48;
+		}
+		return keyCode;
+	}
 
-                        if (keyCode == 37 || keyCode == 39 || keyCode == 18 || keyCode == 9) {
-                                super.onBrowserEvent(event);
-                                return;
-                        } else {
-                                if (isTimeKey(keyCode)) {
-                                        final int cursor = super.getCursorPos();
-                                        if (cursor >= format.length()) {
+	private boolean isTimeKey(int keyCode) {
+		// 0 - 9 keys above qwerty
+		if (keyCode >= 48 && keyCode <= 57) {
+			return true;
+		}
+		// 0 - 9 keys on number pad
+		if (keyCode >= 96 && keyCode <= 105) {
+			return true;
+		}
+		// A, M, P keys
+		if (keyCode == 81 || keyCode == 87 || keyCode == 69 || keyCode == 82
+				|| keyCode == 84 || keyCode == 89 || keyCode == 85
+				|| keyCode == 37 || keyCode == 79 || keyCode == 80) {
+			return true;
+		}
+		return false;
+	}
 
-                                        } else {
-                                                final String temp = super.getText();
-                                                final char rep = format.charAt(cursor);
-                                                if (rep == ':') {
-                                                        super.setCursorPos(cursor + 1);
-                                                } else {
-                                                        char next = (char)-1;
-                                                        if(format.length() > cursor + 1){
-                                                                next = format.charAt(cursor + 1);
-                                                        }
-                                                        char newChar = ((char) convert(keyCode));
-                                                        boolean allow = false;
-                                                        if (rep == 'H' && next == 'H') {
-                                                                if (newChar == '0' || newChar == '1' || newChar == '2') {
-                                                                        allow = true;
-                                                                }
-                                                        } else {
-                                                                if (rep == 'H') {
-                                                                        char prev = temp.charAt(cursor - 1);
-                                                                        if (prev == '2') {
-                                                                                if (newChar == '0' || newChar == '1' || newChar == '2' || newChar == '3') {
-                                                                                        allow = true;
-                                                                                }
-                                                                        } else {
-                                                                                allow = true;
-                                                                        }
+	public String getFormat() {
+		return format;
+	}
 
-                                                                }
-                                                        }
+	public void setFormat(String format) {
+		this.format = format;
+		super.setMaxLength(format.length());
+		super.setVisibleLength(format.length());
 
-                            if (rep == 'h' && next == 'h') {
-                                if (newChar == '0' || newChar == '1') {
-                                    allow = true;
-                                }
-                            } else {
-                                if (rep == 'h') {
-                                    char prev = temp.charAt(cursor - 1);
-                                    if (prev == '1') {
-                                        if (newChar == '0' || newChar == '1' || newChar == '2') {
-                                            allow = true;
-                                        }
-                                    } else {
-                                        allow = true;
-                                    }
-
-                                }
-                            }
-                                                       
-                                                        if (rep == 'm' && next == 'm') {
-                                                                if (newChar == '0' || newChar == '1' || newChar == '2' || newChar == '3' || newChar == '4' || newChar == '5' || newChar == '6') {
-                                                                        allow = true;
-                                                                }
-                                                        } else {
-                                                                if (rep == 'm') {
-                                                                        final char prev = temp.charAt(cursor - 1);
-                                                                        if (prev == '0' || prev == '1' || prev == '2' || prev == '3' || prev == '4' || prev == '5') {
-                                                                                allow = true;
-                                                                        } else {
-                                                                                if (newChar == '0') {
-                                                                                        allow = true;
-                                                                                }
-                                                                        }
-                                                                }
-                                                        }
-
-                            if (rep == 'a' && next == 'a') {
-                                if (newChar == 'A' || newChar == 'a') {
-                                    newChar = 'A';
-                                    allow = true;
-                                } else if (newChar == 'P' || newChar == 'p') {
-                                    newChar = 'P';
-                                    allow = true;
-                                }
-
-                            } else {
-                                if (rep == 'a') {
-                                    if (newChar == 'M' || newChar == 'm') {
-                                        newChar = 'M';
-                                        allow = true;
-                                    }
-                                }
-                            }
-
-                            if (allow) {
-                                                                final String newString = temp.substring(0, cursor) + newChar + temp.substring(cursor + 1, format.length());
-                                                                super.setText(newString);
-                                                                if (next == ':') {
-                                                                        super.setCursorPos(cursor + 2);
-                                                                } else {
-                                                                        super.setCursorPos(cursor + 1);
-                                                                }
-
-                                                        }
-
-                                                }
-                                        }
-                                        DOM.eventPreventDefault(event);
-                                        return;
-
-                                } else {
-                                        DOM.eventPreventDefault(event);
-                                        return;
-                                }
-
-                        }
-
-                }
-                default:
-                        super.onBrowserEvent(event);
-                }
-        }
-
-        private int convert(int keyCode) {
-        // convert 0 - 9 keys on number pad down into normal 0 - 9 range  
-        if (keyCode >= 96 && keyCode <= 105) {
-            return keyCode - 48;
-        }
-        return keyCode;
-        }
-
-        private boolean isTimeKey(int keyCode) {
-            // 0 - 9 keys above qwerty
-                if (keyCode >= 48 && keyCode <= 57) {
-                        return true;
-                }
-                // 0 - 9 keys on number pad
-        if (keyCode >= 96 && keyCode <= 105) {
-            return true;
-        }
-        // A, M, P keys
-        if (keyCode == 65 || keyCode == 77 || keyCode == 80) {
-            return true;
-        }
-                return false;
-        }
-
-      
-
-        public String getFormat() {
-                return format;
-        }
-
-        public void setFormat(String format) {
-                this.format = format;
-                super.setText(format);
-                super.setMaxLength(format.length());
-                super.setVisibleLength(format.length());
-
-        }
+	}
 }
-
